96351be8ff42b5bb602e4467525e44d0b777f3d7
Orbital <orbitalturtle@protonmail.com>
2024-04-08 15:37:30 -0500

    client: add spam_appointments command

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use bitcoin::secp256k1::SecretKey;
 use plugin::net::http;
 use std::env;
+use std::str::FromStr;
 use teos_common::net::NetAddr;
-use teos_common::test_utils::get_random_user_id;
+use teos_common::cryptography;
+use teos_common::test_utils::{generate_random_appointment, get_random_user_id};
+use tokio::{select, time, time::Duration};
 
 #[tokio::main]
 async fn main() {
@@ -21,11 +25,19 @@ async fn main() {
                     println!("ERROR: Usage for register: register <tower_id> <address>")
                 }
             },
-            "add_appointment" => add_appointment(),
-            _ => println!("ERROR: Unknown command. Please use 'register' or 'add_appointment'."),
+            "spam_appointments" => {
+                if args.len() == 4 {
+                    let address = args[2].clone();
+                    let sk = SecretKey::from_str(&args[3].clone()).unwrap();
+                    spam_appointments(address, sk).await;
+                } else {
+                    println!("ERROR: Usage for spam_appointments: spam_appointments <address> <sk>")
+                }
+            },
+            _ => println!("ERROR: Unknown command. Please use 'register' or 'spam_appointments'."),
         }
     } else {
-        println!("ERROR: No command provided. Please specify 'register' or 'add_appointment'.");
+        println!("ERROR: No command provided. Please specify 'register' or 'spam_appointments'.");
     }
 }
 
@@ -44,8 +56,29 @@ async fn register(_tower_id: String, addr: String) {
     };
 }
 
-// Placeholder function for the "add_appointment" command.
-fn add_appointment() {
+// Placeholder function for the "spam_appointments" command.
+async fn spam_appointments(addr: String, sk: SecretKey) {
     println!("Adding an appointment...");
-    // Implement the add_appointment logic here
+
+    let tower_id = get_random_user_id();
+    let tower_net_addr = NetAddr::new(format!("{addr}"));
+
+    let appointment = generate_random_appointment(None);
+    let signature = cryptography::sign(
+        &appointment.to_vec(),
+        &sk,
+    )
+    .unwrap();
+
+    let mut interval = time::interval(Duration::from_secs(1));
+    loop {
+        select!(
+            _ = interval.tick() => {
+                match http::add_appointment(tower_id, &tower_net_addr, &None, &appointment, &signature).await {
+                    Ok(_) => println!("SUCCESS adding appointment"),
+                    Err(e) => println!("ERROR adding appointment: {e:?}"),
+                };
+            }
+        )
+    }
 }
